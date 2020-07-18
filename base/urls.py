ecd34c53fc41354dd90c35255fdf00d9413ce85e
Jon Lee <39420293+4ka0@users.noreply.github.com>
2020-07-18 15:58:00 +0100

    Added user authentication, pages app, and some tests

--- base/urls.py
+++ base/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
+    # For the admin page
     path('admin/', admin.site.urls),
+    # To include the pages app
+    path('', include('pages.urls')),
+    # To include the users app from the '/users' url
+    path('users/', include('users.urls')),
+    # To include the built-in auth app (has its own urls for login and logout)
+    path('users/', include('django.contrib.auth.urls')),
+
 ]
--- users/forms.py
+++ users/forms.py
@@ -8,11 +8,21 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+        '''
+        Meta.fields just displays the default settings of username and password,
+        to which we then add an age field
+        '''
+        # fields = UserCreationForm.Meta.fields + ('age',)
+
+        '''
+        This approach specifies exactly which fields to use
+        '''
+        fields = ('username', 'email', 'age')
 
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        # fields = UserChangeForm.Meta.fields
+        fields = ('username', 'email', 'age')
