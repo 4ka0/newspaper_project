4480c0b2f4e05cd3ba80a7e05b0c93ae03501cf5
Jon Lee <39420293+4ka0@users.noreply.github.com>
2020-07-22 19:33:04 +0100

    Added article edit and delete functionality

--- articles/urls.py
+++ articles/urls.py
@@ -1,8 +1,23 @@
 from django.urls import path
 
-from .views import ArticleListView
+from .views import (
+    ArticleListView,
+    ArticleUpdateView,
+    ArticleDetailView,
+    ArticleDeleteView,
+)
 
+
+'''
+To edit and delete articles we need new urls, views, and templates. For the
+urls we can take advantage of the fact that Django automatically adds a primary
+key to each database. Therefore our first article with a primary key of 1 will
+be at articles/1/edit/ and the delete route will be at articles/1/delete/.
+'''
 
 urlpatterns = [
+    path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('<int:pk>/edit', ArticleUpdateView.as_view(), name='article_edit'),
+    path('<int:pk>/delete', ArticleDeleteView.as_view(), name='article_delete'),
     path('', ArticleListView.as_view(), name='article_list')
 ]
