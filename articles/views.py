9c94566672b1b3c9012171390e1715fc4e9c868c
Jon Lee <39420293+4ka0@users.noreply.github.com>
2020-07-23 09:58:58 +0100

    Updated articles view to include authorization

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
@@ -5,29 +6,52 @@ from django.urls import reverse_lazy
 from .models import Article
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = 'article_list.html'
+    login_url = 'login'
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = 'article_detail.html'
+    login_url = 'login'
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ('title', 'body',)
     template_name = 'article_edit.html'
+    login_url = 'login'
+
+    # Test to see if the current user is the author of the current article.
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+    login_url = 'login'
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
 
-class ArticleCreateView(CreateView):
+'''
+Uses LoginRequiredMixin to restrict access to this view to only users who are
+logged in. Make sure to include LoginRequiredMixin before CreateView.
+'''
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = 'article_new.html'
-    fields = ('title', 'body', 'author')
+    fields = ('title', 'body')
+    login_url = 'login'  # overrides the default login url (/accounts/login)
+
+    # Set the current user as the author of a new article
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
