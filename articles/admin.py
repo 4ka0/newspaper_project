3e3f8157ca932d5eb6214af29b152c056de2bc70
Jon Lee <39420293+4ka0@users.noreply.github.com>
2020-07-23 11:52:11 +0100

    Added comment functionality

--- articles/admin.py
+++ articles/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Comment
 
 
-admin.site.register(Article)
+'''
+The following two classes make it possible to see associated comments when
+looking at a specific article on the admin page.
+'''
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
--- articles/models.py
+++ articles/models.py
@@ -18,3 +18,34 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         return reverse('article_detail', args=[str(self.id)])
+
+
+class Comment(models.Model):
+    '''
+    The next line links the comment model to the article model through the use
+    of a foreign key, and also sets a many-to-one relationship from the comment
+    model to te article model. It requires two positional arguments: the other
+    model to which this model is related and the on_delete option.
+    '''
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE,
+        related_name='comments'  # Used in the template, in the template {% for comment in article.comments.all %}
+    )
+
+    '''
+    This line links the comment model to the custom user model in a similar
+    manner to the above.
+    '''
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+
+    comment = models.CharField(max_length=140)
+
+    def __str__(self):
+        return self.comment
+
+    def get_absolute_url(self):
+        return reverse('article_list')
