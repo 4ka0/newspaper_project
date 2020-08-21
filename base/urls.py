24b38591024d803fff1000275bde5dc64a1a708f
Jon Lee <39420293+4ka0@users.noreply.github.com>
2020-08-21 12:10:13 +0100

    Added image uploading functionality

--- base/urls.py
+++ base/urls.py
@@ -1,17 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-# from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
-    # For the admin page
     path('admin/', admin.site.urls),
-    # To include the pages app
     path('', include('pages.urls')),
-    # To include the users app from the '/users' url
-    path('users/', include('users.urls')),
-    # To include the built-in auth app (has its own urls for login and logout)
-    path('users/', include('django.contrib.auth.urls')),
-    # To include the articles app
+    path('users/', include('users.urls')),  # To include the users app at the '/users' url
+    path('users/', include('django.contrib.auth.urls')),  # To include the built-in auth app (has its own urls for login and logout)
     path('articles/', include('articles.urls')),
 ]
